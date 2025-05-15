69fe1c841e14a176651f1953def233d936e458fc
Dwane Dunn <dwane.dunn@gmail.com>
2025-05-15 19:06:00 +0000

    build Contacts

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -2,5 +2,17 @@ import React from 'react';
 import storeReducer from '../store';
 
 export const Contacts = () => {
-  return <p>Hello Contacts</p>;
+  return (
+    <ul>
+      {store.contacts.map((contact) => {
+        return (
+          <li key={contact.id}>
+            <h2>{contact.name}</h2>
+            <p>{contact.email}</p>
+            <p>{contact.phone}</p>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
