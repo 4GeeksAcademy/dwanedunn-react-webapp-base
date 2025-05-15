8d4bba56f2347264e13aed48b06dc6d68032a969
Dwane Dunn <dwane.dunn@gmail.com>
2025-05-15 19:18:27 +0000

    setup contacts

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import storeReducer from '../store';
 
 export const Contacts = () => {
+  const { store, dispatch } = useGlobalReducer();
   return (
     <ul>
       {store.contacts.map((contact) => {
