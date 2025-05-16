aaba1f2c9aac96b27a6ac38de03d400e72cbab13
Dwane Dunn <dwane.dunn@gmail.com>
2025-05-16 21:51:33 +0000

    fix imports

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import storeReducer from '../store';
 
 // Import necessary components from react-router-dom and other parts of the application.
